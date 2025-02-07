95e42b6393f9715cd9de024694be0deb01127da3
nzbin <nzb329@163.com>
2025-02-07 14:57:14 +0800

    feat: add start and package command

--- bin/adviz.js
+++ bin/adviz.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
-"use strict";
+'use strict';
 
+import AdmZip from 'adm-zip';
+import { spawn } from 'child_process';
+import { program } from 'commander';
 import { readJsonFromRoot } from '../lib/utils.js';
-import { program, Option } from 'commander';
 
 const npmPackage = readJsonFromRoot('package.json');
 const rootPath = process.cwd();
@@ -14,8 +16,47 @@ const adviz = program
   .addHelpText('beforeAll', `${npmPackage.name} version - ${npmPackage.version}`);
 
 adviz
-  .command('info')
-  .description('Displays visual info')
-  .action(() => {});
+  .command('start')
+  .usage('<argument> [options]')
+  .argument('<name>', 'The name of the project to build.')
+  .action((name, options) => {
+    const ng = spawn('ng', ['serve', name]);
+    ng.stdout.on('data', data => {
+      console.log(`${data}`);
+    });
+    ng.stderr.on('data', data => {
+      console.error(`${data}`);
+    });
+    ng.on('close', code => {});
+  });
+
+adviz
+  .command('package')
+  .usage('<argument> [options]')
+  .argument('<name>', 'The name of the project to package.')
+  .action((name, options) => {
+    const ng = spawn('ng', ['build', name]);
+    ng.stdout.on('data', data => {
+      console.log(`${data}`);
+    });
+    ng.stderr.on('data', data => {
+      console.error(`${data}`);
+    });
+    ng.on('close', code => {
+      if (code == 0) {
+        const zip = new AdmZip();
+        zip.addLocalFolder(rootPath + `/dist/${name}`);
+        zip.writeZip(rootPath + `/dist/${name}.zip`, () => {
+          console.log('Package created.');
+        });
+      }
+    });
+  });
+
+adviz
+  .command('publish')
+  .usage('<argument> [options]')
+  .argument('<name>', 'The name of the project to publish.')
+  .action((name, options) => {});
 
 program.parse(process.argv);
